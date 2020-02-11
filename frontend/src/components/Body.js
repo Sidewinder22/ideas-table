3fe4e32a368dbb7ae520519d56f5746ce11b8a64
Damian Stępień <damian.stepien@harman.com>
2020-02-11 14:26:43 +0100

    Frontend deployment settings

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import '../index.css';
 
-const API = 'http://127.0.0.1:5000/api/ideas/' ;
+//const API = 'http://127.0.0.1:5000/api/ideas/' ;
+const API = 'http://167.172.183.173:5000/api/ideas/' ;
 
 class Body extends Component {
     constructor(props) {
