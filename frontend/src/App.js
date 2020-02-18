38b3e484da4a4c2b0a1c8d9010956ddf98195954
Damian Stępień <damian.stepien@harman.com>
2020-02-18 13:59:02 +0100

    Add WelcomeScreen component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Nav } from './grid_elements/Nav';
 import { Footer } from './grid_elements/Footer';
 import { Main } from './grid_elements/Main';
 import { LoginScreen } from './components/LoginScreen';
+import { WelcomeScreen } from './components/WelcomeScreen';
 
 export const API = 'http://127.0.0.1:5000/api/' ;
 // export const API = 'https://ideas.api.sidewinder22.pl/api/' ;
@@ -31,10 +32,10 @@ class App extends Component {
       this.handleSpecificCategoryClick.bind(this);
     this.handleCleanSpecificCategoryClick =
       this.handleCleanSpecificCategoryClick.bind(this);
-    this.handleSingUpSubmit = 
-      this.handleSingUpSubmit.bind(this);
-    this.handleSingInSubmit = 
-      this.handleSingInSubmit.bind(this);
+    this.handleSingUpClick = 
+      this.handleSingUpClick.bind(this);
+    this.handleSingInClick = 
+      this.handleSingInClick.bind(this);
   }
 
   componentDidMount() {
@@ -57,34 +58,13 @@ class App extends Component {
     this.mainElement.current.cleanSpecificCategory();
   }
 
-  handleSingUpSubmit(event) {
-    console.log(`handleSingUpSubmit: ${event}`)
+  handleSingUpClick(event) {
+    console.log(`handleSingUpClick: ${event}`)
 
-    event.preventDefalut();
-
-    // fetch(API + USERS_QUERY, {
-    //   method: 'POST',
-    //   headers: {
-    //       'Accept': 'application/json',
-    //       'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //       title: 'New Idea',
-    //       user_id: this.props.user_id,
-    //   })
-    //   })
-    //   .then(response => response.json())
-    //   .then(idea => {
-    //       let newIdeas = this.state.ideas;
-    //       let ideaObject = JSON.parse(idea)
-
-    //       newIdeas.push(ideaObject)
-    //       this.setState({ ideas: newIdeas})
-    //   });
   }
 
-  handleSingInSubmit(event) {
-    console.log(`handleSingInSubmit`)
+  handleSingInClick(event) {
+    console.log(`handleSingInClick`)
   }
 
   render() {
@@ -98,11 +78,15 @@ class App extends Component {
           onCleanSpecificCategoryClick = { this.handleCleanSpecificCategoryClick }
         />
 
-        {/* if user LoginScreen else Main */}
-        <LoginScreen
-          onSignUpSubmit = { this.handleSingUpSubmit }
-          onSignInSubmit = { this.handleSingInSubmit }
+        <WelcomeScreen
+          onSignUpClick = { this.handleSingUpClick }
+          onSignInClick = { this.handleSingInClick }
         />
+
+        {/* <LoginScreen
+          onSignUpClick = { this.handleSingUpClick }
+          onSignInClick = { this.handleSingInClick }
+        /> */}
         
         {/* <Main 
           ref = { this.mainElement }
